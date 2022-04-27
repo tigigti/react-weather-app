57e87d7d4448f8a95166bcacf1bec09b41aec0dd
Angelos Ioannou <angelos.ioannou@gmx.de>
2022-04-27 19:08:21 +0200

    initial commit, api calls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { locationiqKey, weatherKey } from "./keys";
 
 function App() {
+  const [cityName, setCityName] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const lonlat = await forwardGeocoding(cityName);
+
+    const weatherReport = await getWeather(lonlat[0], lonlat[1]);
+    console.log(weatherReport);
+  }
+
+  function forwardGeocoding(cityName) {
+    return new Promise(resolve => {
+      fetch(`https://eu1.locationiq.com/v1/search.php?key=${locationiqKey}&city=${cityName}&format=json`)
+        .then(res => res.json())
+        .then(res => {
+          const lonlat = [res[0].lon, res[0].lat];
+          resolve(lonlat);
+        });
+    });
+  }
+
+  function getWeather(lon, lat) {
+    return new Promise(resolve => {
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${weatherKey}`)
+        .then(res => res.json())
+        .then(res => resolve(res));
+    });
+  }
+
+  function handleChange(e) {
+    setCityName(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="city-name" value={cityName} onChange={handleChange} />
+        <input type="submit" value="OK" />
+      </form>
     </div>
   );
 }
